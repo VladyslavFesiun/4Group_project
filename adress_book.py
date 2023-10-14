90d26d9d304d2dd401f4ea151f5e07f15a283f0d
VolodymyrTES <135321689+VolodymyrTES@users.noreply.github.com>
2023-10-14 22:03:27 +0300

    classField

--- adress_book.py
+++ adress_book.py
@@ -3,10 +3,15 @@ import re
 
 class Field:
     def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
+        self._value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value 
 
 class Name(Field):
     pass    
