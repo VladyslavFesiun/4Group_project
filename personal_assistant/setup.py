c430fab1248123fba31d9a7a62069b43c812444a
Vladyslav Fesiun <fesyun.vlad@gmail.com>
2023-10-16 20:28:08 +0300

    added sorter

--- personal_assistant/setup.py
+++ personal_assistant/setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(name='personal_assistant',
       version='0.0.1',
-      description='This app can create addressbook and notebook',
-      author='Vladyslav Fesiun',
+      description='This package can create addressbook and notebook, '
+                  'sort your files and generate temp folder with random files',
+      author='4Group',
       license='MIT',
       packages=['personal_assistant'],
-      entry_points={'console_scripts': ['personal-assistant = personal_assistant.adress_book:main']
+      include_package_data=True,
+      install_requires=['numpy', 'Pillow', 'prompt_toolkit'],
+      entry_points={'console_scripts': ['create-books = personal_assistant.adress_book:main',
+                                        'clean-folder = personal_assistant.clean_folder:sort_files',
+                                        'fill-files = personal_assistant.files_generator:file_generator']
                     },
 )
